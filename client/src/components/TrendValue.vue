b4364964a1afb5caff1f56237d96a3983578faf2
milk <milk00000000000000@gmail.com>
2025-01-07 20:32:36 +0100

    [client] trend component

---
+++ client/src/components/TrendValue.vue
@@ -0,0 +1,31 @@
+<template>
+  <p v-if="trend?.status"
+     :class="trendClass[trend.status]"
+  >
+    {{ trendDirection }} {{ trend.value }}
+  </p>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import { trendClass } from '@/utils/html.ts';
+
+import type { Trend } from '@/types/models.ts';
+
+
+interface Props {
+  trend: Trend;
+}
+
+const props = defineProps<Props>();
+
+const trendDirection = computed(() => {
+  return props?.trend?.status === 'up' ? '+' : '-';
+});
+
+</script>
+
+<style scoped>
+
+</style>

f044d8cb2f42e23aa513c6cd2c56223b63126213
milk <milk00000000000000@gmail.com>
2025-01-07 20:32:44 +0100

    [client] price value component

---
+++ client/src/components/PriceValue.vue
@@ -0,0 +1,30 @@
+<template>
+  <p class="text-xl font-semibold tracking-tight leading-none">
+    {{mainPrice}}<span
+      class="text-sm inline-block align-top leading-none">{{ secondaryPrice }}</span>
+  </p>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+
+interface Props {
+  price: number;
+}
+
+const props = defineProps<Props>();
+
+const mainPrice = computed(() => {
+  return props?.price?.toString().slice(0, props.price.toString().length - 1)
+})
+
+const secondaryPrice = computed(() => {
+  return props?.price?.toString().slice(-1)
+})
+
+</script>
+
+<style scoped>
+
+</style>

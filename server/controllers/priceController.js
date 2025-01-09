fb194892fcf2c1cddd7a817f87c3d5d616395b18
milk <milk00000000000000@gmail.com>
2025-01-09 13:07:22 +0100

    [server] change prices aggregation

--- server/controllers/priceController.js
+++ server/controllers/priceController.js
@@ -1,11 +1,45 @@
 import Price from '../mongodb/models/price.js';
-
+import mongoose from 'mongoose';
+const {ObjectId} = mongoose.Types;
 
 export const getPrices = async (req, res) => {
   const {id} = req.params;
+  const {startDay, endDay} = req.query;
+
+  const startDate = new Date(`${startDay}T00:00:00.000Z`);
+  const endDate = new Date(`${endDay}T23:59:59.999Z`);
+
 
   try {
-    const prices = await Price.find({stationId: id});
+    const prices = await Price.aggregate([
+      {
+        $match: {
+          stationId: new ObjectId(id),
+          updatedAt: { $gte: startDate, $lte: endDate },
+        },
+      },
+      { $sort: { updatedAt: -1 } },
+      {
+        $group: {
+          _id: {
+            e10: "$e10",
+            super: "$super",
+            diesel: "$diesel",
+          },
+          firstRecord: { $first: "$$ROOT" },
+          lastRecord: { $last: "$$ROOT" },
+        },
+      },
+      {
+        $project: {
+          records: ["$firstRecord", "$lastRecord"],
+        },
+      },
+      { $unwind: "$records" },
+      { $replaceRoot: { newRoot: "$records" } },
+      { $sort: { updatedAt: 1 } },
+    ]);
+
     res.status(200).json({success: true, data: prices});
   } catch (error) {
     res.status(500).json({success: false, message: error});
